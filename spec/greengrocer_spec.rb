49dc8581c2918dbb18462094110359c4a395d090
rotosiri-zu <hongjitufang25@gmail.com>
2021-05-24 22:11:13 +0900

    単体テスト8完了

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -1,13 +1,14 @@
 RSpec.describe Greengrocer do
+  let(:product_params) do
+    [
+      { name: "トマト", price: 100 },
+      { name: "きゅうり", price: 200 }
+    ]
+  end
+  let(:greengrocer) { Greengrocer.new(product_params) }
+
   # ▼単体テスト2 正常系（Greengrocerクラスのインスタンス生成）
   describe ".initialize" do
-    let(:product_params) do
-      [
-        { name: "トマト", price: 100 },
-        { name: "きゅうり", price: 200 }
-      ]
-    end
-    let(:greengrocer) { Greengrocer.new(product_params) }
     let(:products) { greengrocer.products }
     context "インスタンスが生成されたとき" do
       it "@productsの要素の数が,product_paramsの要素の数と等しいこと" do
@@ -26,13 +27,6 @@ RSpec.describe Greengrocer do
 
   # ▼単体テスト３ 正常系(register_productメソッド)
   describe ".register_product" do
-    let(:product_params) do
-      [
-        { name: "トマト", price: 100 },
-        { name: "きゅうり", price: 200 }
-      ]
-    end
-    let(:greengrocer) { Greengrocer.new(product_params) }
     let(:products) { greengrocer.products }
     let(:adding_product_params) do
       [
@@ -63,23 +57,11 @@ RSpec.describe Greengrocer do
       product_msg1 = "#{base_id + 1}.トマト（¥100円）"
       product_msg2 = "#{base_id + 2}.きゅうり（¥200円）"
       msg = "#{hello_msg}\n#{product_msg1}\n#{product_msg2}\n"
-      product_params = [
-        { name: "トマト", price: 100 },
-        { name: "きゅうり", price: 200 }
-      ]
-      greengrocer = Greengrocer.new(product_params)
       expect { greengrocer.disp_products }.to output(msg).to_stdout
     end
   end
   # ▼単体テスト6 正常系(ask_quantityメソッド)
   describe ".ask_quantity" do
-    let(:product_params) do
-      [
-        { name: "トマト", price: 100 },
-        { name: "きゅうり", price: 200 }
-      ]
-    end
-    let(:greengrocer) { Greengrocer.new(product_params) }
     let(:chosen_product) { Product.new({  name: "玉ねぎ", price: 300 }) }
     let(:ask_msg)  { "玉ねぎですね。何個買いますか？\n" }
     it "userが選択した商品の名前を含む,期待する表示がされること" do
@@ -88,76 +70,46 @@ RSpec.describe Greengrocer do
   end
   # 【単体テスト8 正常系】(calculate_chargesメソッド)
   describe ".calculate_charges" do
+    let(:user) { User.new }
+    let(:thank_msg) { "お買い上げありがとうございました!" }
     
     context "300円の商品(玉ねぎ)が4個のとき" do
+      let(:total_price_msg) { "合計金額は1200円です。" }
       it "正しい合計金額を含む,期待する表情がされること" do
-        product_params =
-        [
-          { name: "トマト", price: 100 },
-          { name: "きゅうり", price: 200 }
-        ]
-        greengrocer = Greengrocer.new(product_params)
-        user = User.new
         user.instance_variable_set("@chosen_product", Product.new({ name: "玉ねぎ", price: 300}))
         user.instance_variable_set("@quantity_of_product", 4)
-        total_price_msg = "合計金額は1200円です。"
-        thank_msg = "お買い上げありがとうございました!"
         expect { greengrocer.calculate_charges(user) }
           .to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
       end
     end
 
     context "400円の商品(なす)が4個のとき" do
+      let(:total_price_msg) { "合計金額は1600円です。" }
       it "正しい合計金額を含む,期待する表情がされること" do
-        product_params =
-        [
-          { name: "トマト", price: 100 },
-          { name: "きゅうり", price: 200 }
-        ]
-        greengrocer = Greengrocer.new(product_params)
-        user = User.new
         user.instance_variable_set("@chosen_product", Product.new({ name: "なす", price: 400}))
         user.instance_variable_set("@quantity_of_product", 4)
-        total_price_msg = "合計金額は1600円です。"
-        thank_msg = "お買い上げありがとうございました!"
         expect { greengrocer.calculate_charges(user) }
           .to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
       end
     end
 
     context "300円の商品(玉ねぎ)が5個のとき" do
+      let(:discount_total_price_msg) { "合計金額は1350円です。" }
+      let(:discount_msg) { "5個以上なので10%割引となります!" }
       it "割引した正しい合計金額を含む,期待する表示がされること" do
-        product_params =
-        [
-          { name: "トマト", price: 100 },
-          { name: "きゅうり", price: 200 }
-        ]
-        greengrocer = Greengrocer.new(product_params)
-        user = User.new
         user.instance_variable_set("@chosen_product", Product.new({ name: "玉ねぎ", price: 300}))
         user.instance_variable_set("@quantity_of_product", 5)
-        discount_msg = "5個以上なので10%割引となります!"
-        discount_total_price_msg = "合計金額は1350円です。"
-        thank_msg = "お買い上げありがとうございました!"
         expect { greengrocer.calculate_charges(user) }
           .to output("#{discount_msg}\n#{discount_total_price_msg}\n#{thank_msg}\n").to_stdout
       end
     end
 
     context "400円の商品(なす)が5個のとき" do
+      let(:discount_total_price_msg) { "合計金額は1800円です。" }
+      let(:discount_msg) { "5個以上なので10%割引となります!" }
       it "割引した正しい合計金額を含む,期待する表示がされること" do
-        product_params =
-        [
-          { name: "トマト", price: 100 },
-          { name: "きゅうり", price: 200 }
-        ]
-        greengrocer = Greengrocer.new(product_params)
-        user = User.new
         user.instance_variable_set("@chosen_product", Product.new({ name: "なす", price: 400}))
         user.instance_variable_set("@quantity_of_product", 5)
-        discount_msg = "5個以上なので10%割引となります!"
-        discount_total_price_msg = "合計金額は1800円です。"
-        thank_msg = "お買い上げありがとうございました!"
         expect { greengrocer.calculate_charges(user) }
           .to output("#{discount_msg}\n#{discount_total_price_msg}\n#{thank_msg}\n").to_stdout
       end
