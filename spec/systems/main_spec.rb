31fab9c5f0af5561c5aa862d8b1e04a228b1bd84
rotosiri-zu <hongjitufang25@gmail.com>
2021-05-26 23:20:18 +0900

    総合テストのlet を使ったリファクタリングのテストまで

--- spec/systems/main_spec.rb
+++ spec/systems/main_spec.rb
@@ -1,47 +1,46 @@
 describe ".grenngrocer_program" do
+  # 商品データ
+  let(:product_params1) do
+    [
+      {name: "トマト", price: 100},
+      {name: "きゅうり", price: 200},
+      {name: "玉ねぎ", price: 300},
+      {name: "なす", price: 400}
+    ]
+  end
+  # product_params1 の商品を持つ八百屋の開店
+  let(:greengrocer1) { Greengrocer.new(product_params1) }
+  # 追加商品データ
+  let(:adding_product_params1) do
+    [
+      {name: "ごぼう", price: 250},
+      {name: "れんこん", price: 350},
+    ]
+  end
+  # 長いので代入
+  let(:products1) { greengrocer1.products }
+  # お客さんの来店
+  let(:user) { User.new }
+  let(:thank_msg) { "お買い上げありがとうございました!" }
   
   context "最初のidの商品が4個のとき" do
+    let(:correct_product_id_input) { "#{products1.first.id}\n" }
+    let(:correct_quantity_input) { "4\n" }
+    let(:total_price_msg) { "合計金額は400円です。" }
     it "正しい合計金額を含む,期待する表示がされること" do
-      # 商品データ
-      product_params1 = [
-        {name: "トマト", price: 100},
-        {name: "きゅうり", price: 200},
-        {name: "玉ねぎ", price: 300},
-        {name: "なす", price: 400}
-      ]
-
-      # product_params1 の商品を持つ八百屋の開店
-      greengrocer1 = Greengrocer.new(product_params1)
-
-      # 追加商品データ
-      adding_products1 = [
-        {name: "ごぼう", price: 250},
-        {name: "れんこん", price: 350},
-      ]
-
       # 商品を登録(adding_products1) の商品を追加
-      greengrocer1.register_product(adding_products1)
-
-      # 長いので代入
-      products1 = greengrocer1.products
-      # お客さんの来店
-      user = User.new
-
+      greengrocer1.register_product(adding_product_params1)
       # 商品を表示
       greengrocer1.disp_products
       # 商品を選択
-      correct_product_id_input = "#{products1.first.id}\n"
       allow(ARGF).to receive(:gets).and_return correct_product_id_input
       user.choose_product(greengrocer1.products)
       # 個数を質問
       greengrocer1.ask_quantity(user.chosen_product)
       # 個数を決定
-      correct_product_id_input = "4\n"
-      allow(ARGF).to receive(:gets).and_return correct_product_id_input
+      allow(ARGF).to receive(:gets).and_return correct_quantity_input
       user.decide_quantity
       # 金額を計算
-      total_price_msg = "合計金額は400円です。"
-      thank_msg = "お買い上げありがとうございました!"
       expect { greengrocer1.calculate_charges(user) }
         .to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
     end
