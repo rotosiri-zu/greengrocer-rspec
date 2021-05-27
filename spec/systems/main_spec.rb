294de2b8372010aa9c27ee1b690d96dddbf7bdb6
rotosiri-zu <hongjitufang25@gmail.com>
2021-05-27 21:35:32 +0900

    総合テスト完了

--- spec/systems/main_spec.rb
+++ spec/systems/main_spec.rb
@@ -23,11 +23,8 @@ describe ".grenngrocer_program" do
   let(:user) { User.new }
   let(:thank_msg) { "お買い上げありがとうございました!" }
   
-  context "最初のidの商品が4個のとき" do
-    let(:correct_product_id_input) { "#{products1.first.id}\n" }
-    let(:correct_quantity_input) { "4\n" }
-    let(:total_price_msg) { "合計金額は400円です。" }
-    it "正しい合計金額を含む,期待する表示がされること" do
+  shared_examples "正しい合計金額を含む,期待する表示がされること" do
+    it do
       # 商品を登録(adding_products1) の商品を追加
       greengrocer1.register_product(adding_product_params1)
       # 商品を表示
@@ -46,18 +43,53 @@ describe ".grenngrocer_program" do
     end
   end
 
-  context "最後のidの商品が4個のとき" do
-    it "正しい合計金額を含む,期待する表示がされること" do
+
+  shared_examples "割引した正しい合計金額を含む,期待する表示がされること" do
+    it do
+      # 商品を登録(adding_products1) の商品を追加
+      greengrocer1.register_product(adding_product_params1)
+      # 商品を表示
+      greengrocer1.disp_products
+      # 商品を選択
+      allow(ARGF).to receive(:gets).and_return correct_product_id_input
+      user.choose_product(greengrocer1.products)
+      # 個数を質問
+      greengrocer1.ask_quantity(user.chosen_product)
+      # 個数を決定
+      allow(ARGF).to receive(:gets).and_return correct_quantity_input
+      user.decide_quantity
+      # 金額を計算
+      expect { greengrocer1.calculate_charges(user) }
+        .to output("#{discount_msg}\n#{discount_total_price_msg}\n#{thank_msg}\n").to_stdout
     end
+  end
+  context "最初のidの商品が4個のとき" do
+    let(:correct_product_id_input) { "#{products1.first.id}\n" }
+    let(:correct_quantity_input) { "4\n" }
+    let(:total_price_msg) { "合計金額は400円です。" }
+    it_behaves_like "正しい合計金額を含む,期待する表示がされること"
+  end
+
+  context "最後のidの商品が4個のとき" do
+    let(:correct_product_id_input) { "#{products1.last.id}\n" }
+    let(:correct_quantity_input) { "4\n" }
+    let(:total_price_msg) { "合計金額は1400円です。" }
+    it_behaves_like "正しい合計金額を含む,期待する表示がされること"
   end
 
   context "最初のidの商品が5個のとき" do
-    it "割引した正しい合計金額を含む,期待する表示がされること" do
-    end
+    let(:correct_product_id_input) { "#{products1.first.id}\n" }
+    let(:correct_quantity_input) { "5\n" }
+    let(:discount_msg) { "5個以上なので10%割引となります!" }
+    let(:discount_total_price_msg) { "合計金額は450円です。" }
+    it_behaves_like "割引した正しい合計金額を含む,期待する表示がされること"
   end
 
   context "最後のidの商品が5個のとき" do
-    it "割引した正しい合計金額を含む,期待する表示がされること" do
-    end
+    let(:correct_product_id_input) { "#{products1.last.id}\n" }
+    let(:correct_quantity_input) { "5\n" }
+    let(:discount_msg) { "5個以上なので10%割引となります!" }
+    let(:discount_total_price_msg) { "合計金額は1575円です。" }
+    it_behaves_like "割引した正しい合計金額を含む,期待する表示がされること" 
   end
 end
